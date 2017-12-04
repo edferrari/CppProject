7d2801dd141b353d140663f3539572d4e7276f36
Simon-Olivier Morneau <smorn026@uottawa.ca>
2017-12-04 01:06:41 -0500

    Started working on QwintoRow, still unfinished
    Not sure about the correct way to use the templates.

--- QwintoA/QwintoA/QwintoRow.h
+++ QwintoA/QwintoA/QwintoRow.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Dice.h"
+template <Dice::Colour::c>
+class QwintoRow {
+public:
+	QwintoRow();
+	Dice::Colour color = c;
+	int[] data;
+	int& operator[](std::size_t idx);
+	int& operator[] const(std::size_t idx);
+};
