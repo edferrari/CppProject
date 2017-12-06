7217b735b623ff2d236e64017003b04fd9309fc6
edward <eferr102@uottawa.ca>
2017-12-06 16:20:56 -0500

    Scoresheet templates

--- QwintoA/QwintoA/Dice.cpp
+++ QwintoA/QwintoA/Dice.cpp
@@ -1,5 +1,16 @@
-
+#include
 
 struct Dice {
 	const Colour c;
 };
+
+int roll() {
+	std::default_random_engine e;
+	for (rolls i = 1; i <= 6; i++){
+		e.min();
+		e.max();
+	}
+
+	int face = rand(6);
+	return face;
+}
--- QwintoA/QwintoA/Main.cpp
+++ QwintoA/QwintoA/Main.cpp
@@ -1,21 +1,33 @@
-/*
-~~ Main Loop Pseudo Code [4] ~~
-Ask player to choose game version, number of players and names of
-players.
-Create the corresponding players and RollOfDice for the game.
-while end condition is not reached
-	next player takes a turn i.e., becomes active
-	get input from active player before roll
-	roll dice and show result
-	print scoresheet of active player
-	get input from active player after roll
-	score dice according to input from active player
-	loop over all non-active players
-		print scoresheet of non-active player
-		get input from non-active player
-		score dice according to input
-loop over all players
-	calculate points for player
-	print scoresheet
-print overall winner
-*/
+//Ask player to choose game version, number of players and names of players.
+
+//Create the corresponding players and RollOfDice for the game.
+
+//while end condition is not reached
+
+//	next player takes a turn i.e., becomes active
+
+//	get input from active player before roll
+
+//	roll dice and show result
+
+//	print scoresheet of active player
+
+//	get input from active player after roll
+
+//	score dice according to input from active player
+
+//	loop over all non-active players
+
+//		print scoresheet of non-active player
+
+//		get input from non-active player
+
+//		score dice according to input
+
+//loop over all players
+
+//	calculate points for player
+
+//	print scoresheet
+
+//print overall winner
