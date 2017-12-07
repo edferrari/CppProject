32d0348a797fcab6e15e95c9a4543c25ae82bb42
Simon-Olivier Morneau <smorn026@uottawa.ca>
2017-12-06 22:50:18 -0500

    Started ScoreSheet.h and Player.h

--- QwintoA/QwintoA/Player.h
+++ QwintoA/QwintoA/Player.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "RollOfDice.h"
+#include <string>
+
+class Player {
+public:
+	bool isActive;
+	virtual Player() = 0;
+	virtual std::string inputBeforeRoll(&RollOfDice roll) = 0;
+	virtual std::string inputAfterRoll(&RollOfDice roll) = 0;
+}
--- QwintoA/QwintoA/ScoreSheet.h
+++ QwintoA/QwintoA/ScoreSheet.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <string>
+#include "Dice.h"
+#include "RollOfDice.h"
+
+class Scoresheet {
+public:
+	virtual Scoresheet() = 0;
+	std::string playerName;
+	int failed;
+	int score;
+
+	bool virtual score(RollOfDice roll, Dice::Colour color, int position = -1);
+	int virtual setTotal();
+
+	friend virtual std::ostream& operator<<(std::ostream& os, const Scoresheet& scoresheet);
+protected:
+	bool virtual validate(RollOfDice roll, Dice::Colour color, int position = -1) = 0;
+	void virtual calcTotal() = 0;
+}
